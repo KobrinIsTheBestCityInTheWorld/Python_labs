acf1efac0d8a1c5731a20d4a8bce357c6b61abe4
Egor <bsnkqp@gmail.com>
2020-04-12 21:16:31 +0300

    Final variant

--- task7.py
+++ task7.py
@@ -1,17 +1,25 @@
 import math
-
+import sys
 
 def leonardo (n):
 	if not isinstance(n, int):
 		raise NameError('n must be int')
-	if n<0 :
+	if n < 0:
 		raise ValueError('n must be positive')
 	if n == 0 or n == 1:
 		return 1
 	b = (1 + math.sqrt(5))/2
-	number = 2*(b**(n + 1) - (1 - b)**(n + 1))/(b - (1 - b)) - 1
+	number = 2*(b**(n+1) - (1-b)**(n+1))/(b - (1-b)) - 1
 	return int (number)
 
+def main ():
+	while True: 
+		print ("Enter the number of Leonardo's number")
+		n = int(input())
+		print (leonardo (n))
+		print ("One more time?")
+		if input() == "No": 
+			break
 
 if __name__ == '__main__':
-  	print (leonardo(3))
+  	main()
