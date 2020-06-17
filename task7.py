12714c4a9621442d7f78a21abdb914999603ec66
Egor <bsnkqp@gmail.com>
2020-06-17 17:50:39 +0300

    Добавил Args и строки документации(Окончательный вариант задания)

--- task7.py
+++ task7.py
@@ -1,7 +1,16 @@
 import math
 import sys
+import argparse
 
-def leonardo (n):
+def leonardo(n):
+	"""Вычесляет число Леонардо
+
+
+	Args: n -- номер числа, которое нужно вычеслить
+	Return: n-ое число Леонардо
+
+
+	"""
 	if not isinstance(n, int):
 		raise NameError('n must be int')
 	if n < 0:
@@ -10,19 +19,29 @@ def leonardo (n):
 		return 1
 	b = (1 + math.sqrt(5))/2
 	number = 2*(b**(n+1) - (1-b)**(n+1))/(b - (1-b)) - 1
-	return int (number)
+	return int(number)
 
 def main ():
-	while True: 
-		print ("Enter the number of Leonardo's number")
-		n = int(input())
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-n", type=int)
+	args = parser.parse_args()
+	if args.n is not None:
 		try:
-			print (leonardo (n))
+			print(leonardo(args.n))
 		except: 
-			print ("To big number")
-		print ("One more time?")
-		if input() == "No": 
+			print("Error")
+		print("One more time?")
+		if input() in ["No","NO","no","nO","Нет","нет"]: 
+			sys.exit()
+	while True: 
+		n = input("Enter the number: ")
+		try:
+			print(leonardo(int(n)))
+		except: 
+			print("Error")
+		print("One more time?")
+		if input() in ["No","NO","no","nO","Нет","нет"]: 
 			break
 
 if __name__ == '__main__':
-  	main()
+	main()
