870a01a4f888d82fe52cc295176b4830eddc1a38
Egor <bsnkqp@gmail.com>
2020-06-17 21:17:13 +0300

    строки документирования + args(final)

--- task2.py
+++ task2.py
@@ -2,16 +2,35 @@ import random
 import string
 from alive_progress import alive_bar
 import time
+import argparse
 
 
 def words(size):
+	"""Генерирует случайную послеловательность символов
+
+
+	Args: size -- длина последовательности (слова)
+	Return: word -- послеловательность символов (слово)
+ 
+
+	"""
 	word = ''
 	for i in range(size):
 		word += (random.choice(list(string.ascii_letters)))
 	return word
 
-def creat_file(Mb, K=(10,100), L=(3,10)):
-	if isinstance(Mb, (int, float)) and isinstance(K, tuple) and isinstance(L, tuple) and len(K) == 2 and len(L) == 2:
+
+def creat_file(Mb, K, L):
+	"""Генерирует файл со словами
+
+
+	Args: Mb -- размер файла
+		  K -- диапозон, в котором определяется количество слов в строке
+		  L -- диапозон, в котором определяется длина каждого слова
+ 
+
+	"""
+	if isinstance(Mb, int) and isinstance(K, tuple) and isinstance(L, tuple) and len(K) == 2 and len(L) == 2:
 		Mb = 1024*1024*Mb
 		f = open("text.txt", "w")
 		i = 0
@@ -31,6 +50,14 @@ def creat_file(Mb, K=(10,100), L=(3,10)):
 	else:
 		print("Error") 
 
+def main ():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-mb", type=int)
+	parser.add_argument("-k", type=int, nargs="*", default=(10,100))
+	parser.add_argument("-l", type=int, nargs="*", default=(3,10))
+	args = parser.parse_args()
+	creat_file(args.mb, tuple(args.k), tuple(args.l))
+	
 
 if __name__ == '__main__':
-	creat_file(1)
+	main()
